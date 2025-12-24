d5dc0e363b12a524c58bf79bc49ee17bbe593230
Hendrik Gani <hendrik.gani@gmail.com>
2025-12-24 12:56:29 +1100

    Remove griddler references

--- config/initializers/imap.rb
+++
@@ -1,8 +0,0 @@
-module Griddler
-  module Imap
-    class Adapter
-    end
-  end
-end
-
-Griddler.adapter_registry.register(:imap, Griddler::Imap::Adapter)
--- config/initializers/pop3.rb
+++
@@ -1,8 +0,0 @@
-module Griddler
-  module Pop3
-    class Adapter
-    end
-  end
-end
-
-Griddler.adapter_registry.register(:imap, Griddler::Pop3::Adapter)
